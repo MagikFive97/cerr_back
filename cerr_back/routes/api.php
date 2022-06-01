d6a811478ae1d75bdf68b7cad954ddb07a356785
EduardoVaRo <eduardovaro02@gmail.com>
2022-06-01 21:46:45 +0200

    Routa ejemplo de Parte Trabajo

--- cerr_back/routes/api.php
+++ cerr_back/routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/gerencia/clientes',[ClientController::class, 'getAll']);
+
+Route::get('/gerencia/partesTrabajo', [ParteTrabajoController::class, 'getAll']);
