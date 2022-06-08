3dd0ed12d1b2084222b1ee8422a183c01f3cc0a9
fran <fran_97almoradi@hotmail.com>
2022-06-08 08:19:57 +0200

    anadida la parte del login con token y laravel passport, logout , register con el autentificador para que si no estas logueado no puedas acceder a ningun lado

--- cerr_back/routes/api.php
+++ cerr_back/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ParteTrabajoController;
+use App\Http\Controllers\passportAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,35 +17,48 @@ use App\Http\Controllers\ParteTrabajoController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('gerencia')->group(function () {
-    Route::prefix('clientes')->group(function () {
-        Route::get('',[ClientController::class, 'getAll']);
-        Route::get('{id}', [ClientController::class, 'getId']);
-        Route::post('', [ClientController::class, 'crearCliente']);
-        Route::delete('{id}/delete', [ClientController::class , 'deleteCliente']);
-        Route::post('{id}/editar', [ClientController::class, 'editarCliente']);
+*/
+
+
+Route::post('/login',[passportAuthController::class,'authenticate']);
+Route::post('/register',[passportAuthController::class,'register']);
+
+//agregue este middleware para asegurarse de que cada solicitud sea autenticada
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('/logout',[passportAuthController::class,'logout']);
+    Route::get('/date', [passportAuthController::class,'getDetail']);
+
+    Route::prefix('gerencia')->group(function () {
+        Route::prefix('clientes')->group(function () {
+            Route::get('',[ClientController::class, 'getAll']);
+            Route::get('{id}', [ClientController::class, 'getId']);
+            Route::post('', [ClientController::class, 'crearCliente']);
+            Route::delete('{id}/delete', [ClientController::class , 'deleteCliente']);
+            Route::post('{id}/editar', [ClientController::class, 'editarCliente']);
+        });
+
+        Route::prefix('trabajadores')->group(function () {
+            Route::get('',[EmployeeController::class, 'getAllTrabajadores']);
+            Route::get('{id}', [EmployeeController::class, 'getIdTrabajador']);
+            Route::post('', [EmployeeController::class, 'crearTrabajador']);
+            Route::delete('{id}/delete', [EmployeeController::class , 'deleteTrabajador']);
+            Route::post('{id}/editar', [EmployeeController::class, 'editarTrabajador']);
+        });
     });
 
-    Route::prefix('trabajadores')->group(function () {
-        Route::get('',[EmployeeController::class, 'getAllTrabajadores']);
-        Route::get('{id}', [EmployeeController::class, 'getIdTrabajador']);
-        Route::post('', [EmployeeController::class, 'crearTrabajador']);
-        Route::delete('{id}/delete', [EmployeeController::class , 'deleteTrabajador']);
-        Route::post('{id}/editar', [EmployeeController::class, 'editarTrabajador']);
+    Route::prefix('trabajador')->group(function () {
+        Route::prefix('partesTrabajo')->group(function () {
+            Route::get('',[ParteTrabajoController::class, 'getAllPartes']);
+            Route::get('{id}', [ParteTrabajoController::class, 'getIdParte']);
+            Route::post('', [ParteTrabajoController::class, 'crearParte']);
+            Route::delete('{id}/delete', [ParteTrabajoController::class , 'deleteParte']);
+            Route::post('{id}/editar', [ParteTrabajoController::class, 'editarParte']);
+        });
     });
 });
 
-Route::prefix('trabajador')->group(function () {
-    Route::prefix('partesTrabajo')->group(function () {
-        Route::get('',[ParteTrabajoController::class, 'getAllPartes']);
-        Route::get('{id}', [ParteTrabajoController::class, 'getIdParte']);
-        Route::post('', [ParteTrabajoController::class, 'crearParte']);
-        Route::delete('{id}/delete', [ParteTrabajoController::class , 'deleteParte']);
-        Route::post('{id}/editar', [ParteTrabajoController::class, 'editarParte']);
-    });
-});
 
